d007a5c540e74baa5dc88bb9e2d3e0464645ae67
aalm <aalm@gettyimages.com>
2013-02-08 16:33:31 -0800

    Added a stub method to stub an individual cookie.

--- lib/cookie_cutter/test_support/stubs.rb
+++ lib/cookie_cutter/test_support/stubs.rb
@@ -7,8 +7,12 @@ module CookieCutter
         def find(*args)
           new(TestSupport::FakeCookieJar.find)
         end
+
+        def stub
+          new(TestSupport::FakeCookieJar.new)
+        end
       end
-      
+
       def stub_all
         TestSupport::FakeCookieJar.empty!
         CookieCutter::Base::class_eval do
